a3a8bc5da5199569953c26ca3fbd769e56afec91
oshani madhushika <oshani@jobninja.au>
2025-01-28 10:30:18 +0530

    Solved console error

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 // import './App.css';
-import About from './pages/About';
-import Hero from './pages/Hero';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import About from "./pages/About";
+import Hero from "./pages/Hero";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
     <div className="">
-      <Hero/>
-      <About/>
-      {/* <Routes>
-        <Route path="/" element={<Hero />} />
-        
-      </Routes> */}
+      <Hero />
+      <About />
     </div>
-  </Router>
+
+    //   <Router>
+    //   <div className="">
+    //     <Hero/>
+    //     <About/>
+    //     {/* <Routes>
+    //       <Route path="/" element={<Hero />} />
+
+    //     </Routes> */}
+    //   </div>
+    // </Router>
   );
 }
 
